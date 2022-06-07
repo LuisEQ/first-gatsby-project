aba58b644f5a34d363518be66c6652e72f847a9a
LuisEQ <luiseduardoql@hotmail.com>
2022-06-07 02:23:24 -0600

    Part 5 gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
     `gatsby-plugin-mdx`,
   ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,37 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+            data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Fecha de publicación: {node.frontmatter.date}</p>
+                <MDXRenderer>
+                    {node.body}
+                </MDXRenderer>
+            </article>
+            ))
+        }
         </Layout>
     )
 }
-export const query = graphql` {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+export const query = graphql`
+  query {
+  allMdx(sort: {fields: frontmatter___date, order: ASC}) {
     nodes {
-            name
-        }
+      frontmatter {
+        date(formatString: "D MMM, YYYY")
+        title
+      }
+      id
+      body
     }
+  }
 }
 `
 
